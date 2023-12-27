072cdd32696517c7ca71b5af74addef68dcf1d70
Gouranga Satapathy <pulusatapathy@gmail.com>
2023-12-27 22:36:10 +0530

    added modules Repository and corsConfig refactored and blocklist.txt updated

--- src/main/java/com/apiGateWay/ApiGateWay/AppConfig.java
+++ src/main/java/com/apiGateWay/ApiGateWay/AppConfig.java
@@ -1,27 +1,11 @@
 package com.apiGateWay.ApiGateWay;
 
-import org.springframework.context.annotation.Bean;
+import org.example.ModuleConfigurationApp;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({ModuleConfigurationApp.class})
 public class AppConfig {
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfig.setMaxAge(3600L);
-        corsConfig.addAllowedHeader("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }
 }
